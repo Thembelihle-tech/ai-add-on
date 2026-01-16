61e32828aad608d031352b31e38ca92c4fe0f64e
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-16 12:45:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function showAnswer(response){
     console.log(response);
 }
 
-let apiKay = "4686cad4bte466a10731b6f0be84037o";
+let apiKey = "4686cad4bte466a10731b6f0be84037o";
 let context = "be polite and provide a very short answer";
 let prompt = "when was the first AI created?";
 let apiUrl = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
 
 console.log("Processing");
-console.log(apiUrl);
+axios.get(apiUrl).then(showAnswer);
