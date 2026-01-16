bffabdc98c7b6189dd98e19f868c332c562ec602
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-16 12:23:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 let apiKay = "4686cad4bte466a10731b6f0be84037o";
-let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
+let apiUrl = 'https://api.shecodes.io/ai/v1/generate?prompt=&context={context}&key={key}';
