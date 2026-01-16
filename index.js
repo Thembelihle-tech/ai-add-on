2557472cd670e5d1990720f3ca6a84c43b0d4881
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-16 12:26:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 let apiKay = "4686cad4bte466a10731b6f0be84037o";
+let context = "be polite and provide a very short answer";
+let prompt = "when was the first AI created?";
 let apiUrl = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
