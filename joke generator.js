4765ad0ba92be1bbf758ec96168e2133be9f79ec
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-19 22:25:44 +0200

    Update joke generator.js

--- joke generator.js
+++ joke generator.js
@@ -3,6 +3,3 @@ let jokeText = document.querySelector("#joke-text");
 
 buttonElement.addEventListener("click", showJoke);
 
-function showjoke() {
-    jokeText.innerHTML = ""; //Clear previous joke
-}
