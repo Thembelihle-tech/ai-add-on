c21c99cacead2f3c94ea11d9e4486639577f71c5
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-19 12:44:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function showAnswer(response) {
 }
 
 let apiKey = "4686cad4bte466a10731b6f0be84037o";  // Corrected the variable name
-let context = "be polite and provide a very short answer";
-let prompt = "when was the first AI created?";
+let context = "be polite and provide an answer with";
+let prompt = "Who was the first female president?";
 let apiUrl = ` https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}` ;  // Changed to backticks
 
 console.log("Processing");
