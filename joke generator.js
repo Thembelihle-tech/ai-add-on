872a0cfd90bd06122e34b7b53c5318e8dd553935
ThembelihleNkosi1 <nkosit@imentorufoundation.org>
2026-01-19 22:27:58 +0200

    Update joke generator.js

--- joke generator.js
+++ joke generator.js
@@ -1,5 +1,15 @@
 let buttonElement = document.querySelector("#joke");
 let jokeText = document.querySelector("#joke-text");
 
+let jokes = [
+    {
+        question: "Why do French people eat snails?",
+        answer: "Because they don't like fast food 🐌😂",
+    },
+    {
+        
+    }
+]
+
 buttonElement.addEventListener("click", showJoke);
 
